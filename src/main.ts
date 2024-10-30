f62ead5307099323061da705ebb007d0cd983e69
santilaguado <santiagolaguado57@gmail.com>
2024-10-29 19:59:28 -0500

    v0.3.0 - CORS Config, Readme

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useLogger(['error', 'warn', 'log', 'debug', 'verbose']);
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+});
+
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
   }));
+
   await app.listen(3000);
 }
 bootstrap();
